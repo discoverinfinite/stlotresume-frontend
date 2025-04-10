91ba800a7f0093807abbd2a7e6603e9efa9fb521
Pace Mateo <pacemateo7@gmail.com>
2025-04-10 15:55:27 -0400

    Update front-end content

--- resumepj/index.js
+++ resumepj/index.js
@@ -2,7 +2,7 @@ const counter = document.querySelector(".counter-number");
 
 async function updateCounter() {
     try {
-        const response = await fetch("https://mt5ddl5al2ybyyav3vfulhgpfa0yxopb.lambda-url.us-east-1.on.aws/");
+        const response = await fetch("https://xpm14i5onj.execute-api.us-east-1.amazonaws.com/prod");
         const data = await response.json();
         counter.innerHTML = `Page Views: ${data.views}`;
     } catch (error) {
