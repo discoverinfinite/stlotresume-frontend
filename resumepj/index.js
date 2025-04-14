d244814d5a2203bb790ca27531440017b614b2f7
Pace Mateo <pacemateo7@gmail.com>
2025-04-14 19:11:25 -0400

    Fix: Update API fetch URL to include /counter

--- resumepj/index.js
+++ resumepj/index.js
@@ -2,7 +2,7 @@ const counter = document.querySelector(".counter-number");
 
 async function updateCounter() {
     try {
-        const response = await fetch("https://xpm14i5onj.execute-api.us-east-1.amazonaws.com/prod");
+        const response = await fetch("https://xpm14i5onj.execute-api.us-east-1.amazonaws.com/prod/counter");
         const data = await response.json();
         counter.innerHTML = `Page Views: ${data.views}`;
     } catch (error) {
